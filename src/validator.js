47be74da46d0c64ec5a5419754c7658808c1967b
DanielaRoch <116087174+DanielaRoch@users.noreply.github.com>
2022-11-03 19:12:00 -0500

    subir archivos validador

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
+  isValid(creditCardNumber) {
+    const arrayNumber = creditCardNumber.toString().split('').reverse();
+    for (let index = 1; index < arrayNumber.length; index+=2) {
+      var number = arrayNumber[index];
+      number = number * 2;
+      if (number >= 10) {
+        var arraySum = number.toString().split('');
+        number = parseInt(arraySum[0]) + parseInt(arraySum[1]);
+      }
+      arrayNumber[index] = number;
+    }
+    var result = 0;
+    for (let index = 0; index < arrayNumber.length; index++) {
+      result = result + parseInt(arrayNumber[index]);
+    }
+    if (result%10==0) {
+      return true;  
+    }
+    return false;
+  },
+  maskify(creditCardNumber) {
+    const arrayNumber = creditCardNumber.toString().split('');
+    for (let index = 0; index < arrayNumber.length-4 ; index++) {
+      arrayNumber[index] = "#";
+    }
+    return arrayNumber.join('');
+  }
   // ...
 };
 
 export default validator;
+
+      
+    
+
